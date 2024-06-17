93042bdf6b01d08bf4506a28524f2e3bde46d521
Heorhii <heorh.popov@gmail.com>
2024-06-17 20:45:13 +0300

    [LJI-7] Use Lombok @RequiredArgsConstructor in ExampleController

--- src/main/java/com/coherentsolutions/pot/insurance/controller/ExampleController.java
+++ src/main/java/com/coherentsolutions/pot/insurance/controller/ExampleController.java
@@ -4,18 +4,16 @@ import com.coherentsolutions.pot.insurance.entity.ExampleEntity;
 import com.coherentsolutions.pot.insurance.service.ExampleService;
 import java.util.Optional;
 import org.springframework.web.bind.annotation.*;
-
+import lombok.RequiredArgsConstructor;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/examples")
+@RequiredArgsConstructor
 public class ExampleController {
 
   private final ExampleService exampleService;
-
-  public ExampleController(ExampleService exampleService) {
-    this.exampleService = exampleService;
-  }
 
   @PostMapping
   public ExampleEntity createExample(@RequestBody ExampleEntity example) {
