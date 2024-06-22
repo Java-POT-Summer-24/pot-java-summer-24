438b50a87a2465282dc9e121e5bac44e72578a42
Lukas Šimonėlis <118674199+Cioperis@users.noreply.github.com>
2024-06-22 19:54:53 +0300

    updated entity, dto and added mapping using mapstruct

--- src/main/java/com/coherentsolutions/pot/insurance/entity/Employee.java
+++ src/main/java/com/coherentsolutions/pot/insurance/entity/Employee.java
@@ -1,4 +1,35 @@
 package com.coherentsolutions.pot.insurance.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "employee")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Employee {
+    @Id
+    private UUID employeeId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String userName; // unique field
+
+    private Date dateOfBirth;
+
+    private Integer SSN;
+
+    private Integer phoneNumber;
 }
---
+++ src/main/java/com/coherentsolutions/pot/insurance/mappers/EmployeeMapper.java
@@ -0,0 +1,14 @@
+package com.coherentsolutions.pot.insurance.mappers;
+
+import com.coherentsolutions.pot.insurance.dtos.EmployeeDTO;
+import com.coherentsolutions.pot.insurance.entity.Employee;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+@Mapper
+public interface EmployeeMapper {
+    EmployeeMapper INSTANCE = Mappers.getMapper(EmployeeMapper.class);
+
+    EmployeeDTO employeeToEmployeeDTO(Employee employee);
+
+    Employee employeeDTOToEmployee(EmployeeDTO employeeDTO);
+}
