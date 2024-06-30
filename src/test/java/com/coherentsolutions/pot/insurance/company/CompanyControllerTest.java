457b3dd0bfa26d724132db2c57cc9468511903a1
dmytromazurev <dmytro.mazurev@icloud.com>
2024-06-30 13:50:18 +0300

    LJI-5 task. Delete wildcards. Change tests, add Mockmvc

--- src/test/java/com/coherentsolutions/pot/insurance/company/CompanyControllerTest.java
+++ src/test/java/com/coherentsolutions/pot/insurance/company/CompanyControllerTest.java
@@ -3,56 +3,66 @@ package com.coherentsolutions.pot.insurance.company;
 import com.coherentsolutions.pot.insurance.controller.CompanyController;
 import com.coherentsolutions.pot.insurance.dto.CompanyDTO;
 import com.coherentsolutions.pot.insurance.service.CompanyService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.jeasy.random.EasyRandom;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(CompanyController.class)
 class CompanyControllerTest {
 
-    @Mock
-    private CompanyService companyService;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @InjectMocks
-    private CompanyController companyController;
+    @MockBean
+    private CompanyService companyService;
 
     private final EasyRandom easyRandom = new EasyRandom();
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
-    void testCreateCompany() {
+    @WithMockUser(username = "admin")
+    void testCreateCompany() throws Exception {
         CompanyDTO newCompanyDTO = easyRandom.nextObject(CompanyDTO.class);
         when(companyService.addCompany(any(CompanyDTO.class))).thenReturn(newCompanyDTO);
 
-        CompanyDTO createdCompanyDTO = companyController.createCompany(newCompanyDTO);
+        mockMvc.perform(post("/v1/companies")
+                        .with(SecurityMockMvcRequestPostProcessors.csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newCompanyDTO)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(newCompanyDTO)));
 
-        assertEquals(newCompanyDTO, createdCompanyDTO);
         verify(companyService).addCompany(newCompanyDTO);
     }
 
     @Test
-    void testGetAllCompany() {
-        List<CompanyDTO> companyList = easyRandom.objects(CompanyDTO.class, 3).toList();
-        when(companyService.getAllCompanies()).thenReturn(companyList);
-
-        List<CompanyDTO> response = companyController.getAllCompany();
-
-
-        assertEquals(3, response.size());
-        verify(companyService).getAllCompanies();
-    }
-
-    @Test
-    void testUpdateCompany() {
+    @WithMockUser(username = "admin")
+    void testUpdateCompany() throws Exception {
         CompanyDTO originalCompanyDTO = easyRandom.nextObject(CompanyDTO.class);
         UUID id = UUID.randomUUID();
         originalCompanyDTO.setId(id);
@@ -61,23 +71,44 @@ class CompanyControllerTest {
 
         when(companyService.updateCompany(any(CompanyDTO.class))).thenReturn(updatedCompanyDTO);
 
-        CompanyDTO result = companyController.updateCompany(originalCompanyDTO);
+        mockMvc.perform(put("/v1/companies")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(originalCompanyDTO)))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(updatedCompanyDTO)));
 
-        assertEquals(updatedCompanyDTO, result);
         verify(companyService).updateCompany(originalCompanyDTO);
     }
 
     @Test
-    void testDeactivateCompany() {
-        CompanyDTO originalCompanyDTO = easyRandom.nextObject(CompanyDTO.class);
+    @WithMockUser(username = "admin")
+    void testGetAllCompany() throws Exception {
+        List<CompanyDTO> companyList = easyRandom.objects(CompanyDTO.class, 3).toList();
+        when(companyService.getAllCompanies()).thenReturn(companyList);
+
+        mockMvc.perform(get("/v1/companies")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(companyList)));
+
+        verify(companyService).getAllCompanies();
+    }
+
+    @Test
+    @WithMockUser(username = "admin")
+    void testDeactivateCompany() throws Exception {
         UUID id = UUID.randomUUID();
+        CompanyDTO originalCompanyDTO = easyRandom.nextObject(CompanyDTO.class);
         originalCompanyDTO.setId(id);
 
         when(companyService.deactivateCompany(id)).thenReturn(originalCompanyDTO);
 
-        CompanyDTO result = companyController.deactivateCompany(id);
+        mockMvc.perform(delete("/companies/{id}", id)
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()) // Adding CSRF token
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(originalCompanyDTO)));
 
-        assertEquals(originalCompanyDTO.getStatus(), result.getStatus());
         verify(companyService).deactivateCompany(id);
     }
 }
