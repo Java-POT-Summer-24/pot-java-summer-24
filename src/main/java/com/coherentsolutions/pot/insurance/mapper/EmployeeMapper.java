08bc8ab2b09c13d5fab66f23a0dd3989d670cd5f
Lukas Šimonėlis <118674199+Cioperis@users.noreply.github.com>
2024-07-01 22:29:27 +0300

    ignoring id

--- src/main/java/com/coherentsolutions/pot/insurance/mapper/EmployeeMapper.java
+++ src/main/java/com/coherentsolutions/pot/insurance/mapper/EmployeeMapper.java
@@ -3,6 +3,7 @@ package com.coherentsolutions.pot.insurance.mapper;
 import com.coherentsolutions.pot.insurance.dto.EmployeeDTO;
 import com.coherentsolutions.pot.insurance.entity.EmployeeEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
@@ -14,5 +15,6 @@ public interface EmployeeMapper {
 
     EmployeeEntity employeeDTOToEmployee(EmployeeDTO employeeDTO);
 
+    @Mapping(target = "id", ignore = true)
     void updateEmployeeFromDTO(EmployeeDTO dto, @MappingTarget EmployeeEntity entity);
 }
