777e3df95925c910617e3927fd08cf382047da27
Lukas Šimonėlis <118674199+Cioperis@users.noreply.github.com>
2024-07-01 17:05:57 +0300

    added response status

--- src/main/java/com/coherentsolutions/pot/insurance/controller/EmployeeController.java
+++ src/main/java/com/coherentsolutions/pot/insurance/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.coherentsolutions.pot.insurance.controller;
 import com.coherentsolutions.pot.insurance.dto.EmployeeDTO;
 import com.coherentsolutions.pot.insurance.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,26 +16,31 @@ public class EmployeeController {
     private final EmployeeService employeeService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public EmployeeDTO addEmployee(@RequestBody EmployeeDTO employeeDTO){
         return employeeService.addEmployee(employeeDTO);
     }
 
     @GetMapping("/{employeeId}")
+    @ResponseStatus(HttpStatus.OK)
     public EmployeeDTO getEmployee(@PathVariable UUID employeeId){
         return employeeService.getEmployee(employeeId);
     }
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<EmployeeDTO> getAllEmployees(){
         return employeeService.getAllEmployees();
     }
 
     @PutMapping("/{employeeId}")
+    @ResponseStatus(HttpStatus.OK)
     public EmployeeDTO updateEmployee(@PathVariable UUID employeeId, @RequestBody EmployeeDTO updatedEmployeeDTO){
         return employeeService.updateEmployee(employeeId, updatedEmployeeDTO);
     }
 
     @DeleteMapping("/{employeeId}")
+    @ResponseStatus(HttpStatus.OK)
     public void deactivateEmployee(@PathVariable UUID employeeId){
         employeeService.deactivateEmployee(employeeId);
     }
