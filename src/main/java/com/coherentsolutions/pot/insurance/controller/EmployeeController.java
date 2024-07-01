f33022e4927d9dbea14eb79bca631653de58430e
Lukas Šimonėlis <118674199+Cioperis@users.noreply.github.com>
2024-07-01 17:08:55 +0300

    fixed wildcard

--- src/main/java/com/coherentsolutions/pot/insurance/controller/EmployeeController.java
+++ src/main/java/com/coherentsolutions/pot/insurance/controller/EmployeeController.java
@@ -4,10 +4,18 @@ import com.coherentsolutions.pot.insurance.dto.EmployeeDTO;
 import com.coherentsolutions.pot.insurance.service.EmployeeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/employees")
