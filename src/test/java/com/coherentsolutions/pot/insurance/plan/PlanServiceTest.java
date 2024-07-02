e7bb1ff6c9b41d93170aa2081fb48a0d32f3e6bc
NormantasN <80217309+NormantasN@users.noreply.github.com>
2024-07-02 17:35:31 +0300

    Feature/LJI-11-plans-mvc (#19)
    * [LJI-11] Created entity and DTO classes
    
    * [LJI-11] Implemented controller, data, service layers with exception handling on controller layer
    
    * GoogleStyle
    
    * [LJI-11] Created liquibase database script for plans
    
    * [LJI-11] Created tests for plans
    
    * [LJI-11] Implemented corrections based on PR reviews: omitted redundant stuff, added all the generated stuff to .gitignore
    
    * [LJI-11] Corrections for tests
    
    * [LJI-11] Temporarily removed the .idea entry from .gitignore, re-added removed files to git, removed files from Git Tracking, committed changes, re-added .idea entry in .gitignore. Also changed service to return DTO, not ResponseEntity.
    
    * [LJI-11] Added @ResponseStatus for all endpoints, replaced wildcard
    
    * [LJI-11] Added a negative case test for deactivating a non-existant plan
    
    * [LJI-11] Added integration tests
    
    * [LJI-11] Edited exceptions for testDeactivateNonExistingPlan()
    
    * [LJI-11] Edited to have unit tests for the service layer and integration tests for the controller layer, edited PlanEntity to inherit from BaseEntity
    
    * [LJI-11] Edited to receive the ID as a pathVariable to update the particular plan
    
    * [LJI-20] Deleted payroll frequency because Plans cannot have that
    
    * [LJI-20] Deleted payroll frequency from table and inserts
    
    * [LJI-11] Corrected updatePlan method, fixed Postman issues
    
    * [LJI-11] Renamed indexes of create and insert .xml files

---
+++ src/test/java/com/coherentsolutions/pot/insurance/plan/PlanServiceTest.java
@@ -0,0 +1,116 @@
+package com.coherentsolutions.pot.insurance.plan;
+
+
+import com.coherentsolutions.pot.insurance.dto.PlanDTO;
+import com.coherentsolutions.pot.insurance.dto.enums.PlanStatus;
+import com.coherentsolutions.pot.insurance.exception.NotFoundException;
+import com.coherentsolutions.pot.insurance.service.PlanService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.jeasy.random.EasyRandom;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PlanServiceTest {
+
+    private final EasyRandom easyRandom = new EasyRandom();
+
+    @Mock
+    private PlanService planService;
+
+    @Test
+    void testAddPlan() {
+        PlanDTO newPlanDTO = easyRandom.nextObject(PlanDTO.class);
+
+        when(planService.addPlan(any(PlanDTO.class))).thenReturn(newPlanDTO);
+
+        PlanDTO createdPlanDTO = planService.addPlan(newPlanDTO);
+
+        assertEquals(newPlanDTO, createdPlanDTO);
+        verify(planService).addPlan(newPlanDTO);
+    }
+
+    @Test
+    void testGetAllPlans() {
+        List<PlanDTO> plansList = easyRandom.objects(PlanDTO.class, 3).toList();
+        when(planService.getAllPlans()).thenReturn(plansList);
+
+        List<PlanDTO> result = planService.getAllPlans();
+
+        assert result != null;
+        assertEquals(3, result.size());
+        verify(planService).getAllPlans();
+    }
+
+    @Test
+    void testGetPlanById() {
+        PlanDTO PlanDTO = easyRandom.nextObject(PlanDTO.class);
+        UUID id = UUID.randomUUID();
+        PlanDTO.setId(id);
+        when(planService.getPlanById(id)).thenReturn(PlanDTO);
+
+        PlanDTO result = planService.getPlanById(id);
+
+        assert result != null;
+        assertEquals(PlanDTO.getId(), result.getId());
+        verify(planService).getPlanById(id);
+    }
+
+    @Test
+    void testUpdatePlan() {
+        PlanDTO originalPlanDTO = easyRandom.nextObject(PlanDTO.class);
+        PlanDTO updatedPlanDTO = easyRandom.nextObject(PlanDTO.class);
+        updatedPlanDTO.setId(originalPlanDTO.getId());
+
+        UUID planId = originalPlanDTO.getId();
+
+        when(planService.updatePlan(eq(planId), any(PlanDTO.class))).thenReturn(updatedPlanDTO);
+
+        PlanDTO result = planService.updatePlan(planId, originalPlanDTO);
+
+        assertEquals(updatedPlanDTO, result);
+        verify(planService).updatePlan(eq(planId), eq(originalPlanDTO));
+    }
+
+    @Test
+    void testDeactivatePlan() {
+        UUID id = UUID.randomUUID();
+        PlanDTO originalPlanDTO = easyRandom.nextObject(PlanDTO.class);
+        originalPlanDTO.setId(id);
+        originalPlanDTO.setStatus(PlanStatus.DEACTIVATED);
+
+        when(planService.deactivatePlan(id)).thenReturn(originalPlanDTO);
+
+        PlanDTO resultPlanDTO = planService.deactivatePlan(id);
+
+        assert resultPlanDTO != null;
+        assertEquals(PlanStatus.DEACTIVATED, resultPlanDTO.getStatus());
+
+        verify(planService).deactivatePlan(id);
+    }
+
+    @Test
+    void testDeactivateNonExistingPlan() {
+        UUID id = UUID.randomUUID();
+
+        // Mock NotFoundException scenario
+        when(planService.deactivatePlan(id)).thenThrow(new NotFoundException("Plan with id " + id + " not found"));
+
+        // Assert that NotFoundException is thrown
+        NotFoundException exception = assertThrows(NotFoundException.class, () -> {
+            planService.deactivatePlan(id);
+        });
+
+        assertEquals("Plan with id " + id + " not found", exception.getMessage());
+
+        verify(planService).deactivatePlan(id);
+    }
+}
