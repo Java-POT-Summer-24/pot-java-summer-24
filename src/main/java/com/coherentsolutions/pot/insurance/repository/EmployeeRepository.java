ffa96df4b1699bceb00bd314b7ee28b99fda986f
Cioperis <118674199+Cioperis@users.noreply.github.com>
2024-07-05 00:53:17 +0300

    repository now saves specifications

--- src/main/java/com/coherentsolutions/pot/insurance/repository/EmployeeRepository.java
+++ src/main/java/com/coherentsolutions/pot/insurance/repository/EmployeeRepository.java
@@ -2,10 +2,12 @@ package com.coherentsolutions.pot.insurance.repository;
 
 import com.coherentsolutions.pot.insurance.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, UUID>{
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, UUID>,
+    JpaSpecificationExecutor<EmployeeEntity> {
 }
