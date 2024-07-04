d928023f2ed66a63b591ed24c95716c77b694f43
Cioperis <118674199+Cioperis@users.noreply.github.com>
2024-07-05 00:39:39 +0300

    added employee filter criteria

---
+++ src/main/java/com/coherentsolutions/pot/insurance/specifications/EmployeeFilterCriteria.java
@@ -0,0 +1,17 @@
+package com.coherentsolutions.pot.insurance.specifications;
+
+import com.coherentsolutions.pot.insurance.constants.EmployeeStatus;
+import java.time.LocalDate;
+import lombok.Data;
+import org.springdoc.api.annotations.ParameterObject;
+
+@Data
+@ParameterObject
+public class EmployeeFilterCriteria {
+  private String firstName;
+  private String lastName;
+  private String userName;
+  private LocalDate dateOfBirth;
+  private Integer ssn;
+  private EmployeeStatus status;
+}
