4f41ea09a2b924e581dc14e343c23ea34d96e738
Lukas Šimonėlis <118674199+Cioperis@users.noreply.github.com>
2024-07-09 09:34:48 +0300

    resolved repository issue from main

--- src/main/java/com/coherentsolutions/pot/insurance/repository/EmployeeRepository.java
+++ src/main/java/com/coherentsolutions/pot/insurance/repository/EmployeeRepository.java
@@ -1,13 +1,15 @@
 package com.coherentsolutions.pot.insurance.repository;
 
 import com.coherentsolutions.pot.insurance.entity.EmployeeEntity;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<EmployeeEntity, UUID>,
-    JpaSpecificationExecutor<EmployeeEntity> {
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, UUID>{
+
+  Optional<EmployeeEntity> findByUserName(String userName);
+
 }
