337afce795b343007e222dd50b29b26e0df8d62d
Lukas Šimonėlis <118674199+Cioperis@users.noreply.github.com>
2024-07-09 10:20:04 +0300

    fixed cicd bug

--- src/test/java/com/coherentsolutions/pot/insurance/employee/EmployeeIntegrationTest.java
+++ src/test/java/com/coherentsolutions/pot/insurance/employee/EmployeeIntegrationTest.java
@@ -16,8 +16,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -37,6 +39,10 @@ public class EmployeeIntegrationTest {
   private ObjectMapper objectMapper;
   @MockBean
   private EmployeeService employeeService;
+  @BeforeEach
+  void setUp() {
+    MockitoAnnotations.openMocks(this);
+  }
 
   @Test
   void testGetFilteredSortedEmployees() throws Exception {
