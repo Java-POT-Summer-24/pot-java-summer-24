559d1fc5d32fdf12af638a9f76974f92afab51c9
Cioperis <118674199+Cioperis@users.noreply.github.com>
2024-07-15 09:05:52 +0300

    created microservice app within existing app

---
+++ src/microservice/src/test/java/com/coherentsolutions/pot/insurance/microservice/MicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coherentsolutions.pot.insurance.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
