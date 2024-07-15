69eda694798bbf2ca92213c262a60230d96e02a1
dmytromazurev <dmytro.mazurev@icloud.com>
2024-07-15 12:47:24 +0300

    LJI-34 Caching bug fix. Fix the serialization, so now date also can caching.

--- src/main/java/com/coherentsolutions/pot/insurance/config/CacheConfig.java
+++ src/main/java/com/coherentsolutions/pot/insurance/config/CacheConfig.java
@@ -1,6 +1,10 @@
 package com.coherentsolutions.pot.insurance.config;
 
 import java.time.Duration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -12,9 +16,15 @@ public class CacheConfig {
 
     @Bean
     public RedisCacheConfiguration cacheConfiguration() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
         return RedisCacheConfiguration.defaultCacheConfig()
                 .entryTtl(Duration.ofHours(24))
                 .disableCachingNullValues()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
     }
 }
