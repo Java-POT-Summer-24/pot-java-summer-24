b33b5200304eeb904829c7b8b0ea546cb4604ae1
NormantasN <normantas000@gmail.com>
2024-07-17 09:26:56 +0300

    [LJI-35] redis configuration with keycloak TODO: fix "Unable to resolve Configuration with the provided Issuer of "http://localhost:8080/realms/myrealm"

--- src/main/java/com/coherentsolutions/pot/insurance/RedisConfig.java
+++ src/main/java/com/coherentsolutions/pot/insurance/RedisConfig.java
@@ -1,22 +1,36 @@
 package com.coherentsolutions.pot.insurance;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-  @Value("${spring.redis.host}")
+  @Value("${spring.data.redis.host}")
   private String redisHost;
 
-  @Value("${spring.redis.port}")
+  @Value("${spring.data.redis.port}")
   private int redisPort;
 
-  @Value("${spring.redis.password}")
+  @Value("${spring.data.redis.password}")
   private String redisPassword;
 
   @Bean
@@ -27,9 +41,41 @@ public class RedisConfig {
   }
 
   @Bean
-  public RedisTemplate<?, ?> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-    RedisTemplate<byte[], byte[]> template = new RedisTemplate<>();
-    template.setConnectionFactory(redisConnectionFactory);
+  public <T> RedisTemplate<String, T> redisTemplate(@Autowired LettuceConnectionFactory lettuceConnectionFactory) {
+    RedisTemplate<String, T> template = new RedisTemplate<>();
+    template.setConnectionFactory(lettuceConnectionFactory);
+
+    StringRedisSerializer keySerializer = new StringRedisSerializer();
+    RedisSerializer<Object> valueSerializer = RedisSerializer.json();
+
+    template.setKeySerializer(keySerializer);
+    template.setValueSerializer(valueSerializer);
+
+    template.setHashKeySerializer(keySerializer);
+    template.setHashValueSerializer(valueSerializer);
+
+    template.afterPropertiesSet();
     return template;
+  }
+  @Bean
+  public RedisCacheManager redisCacheManager(RedisConnectionFactory factory) {
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.enable(JsonGenerator.Feature.IGNORE_UNKNOWN);
+    objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    objectMapper.registerModule(new JavaTimeModule());
+    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    objectMapper.activateDefaultTyping(objectMapper.getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+
+    RedisSerializer<Object> serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
+
+
+    RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+        .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
+        .entryTtl(Duration.ofHours(24));
+
+    return RedisCacheManager.builder(factory)
+        .cacheDefaults(config)
+        .build();
   }
 }
