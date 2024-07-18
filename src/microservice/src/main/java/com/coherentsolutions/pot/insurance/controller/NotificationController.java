e5fc651fd671c79d16523411cf216a00a55bf1d3
Lukas Šimonėlis <118674199+Cioperis@users.noreply.github.com>
2024-07-18 18:44:10 +0300

    now using lombok + fixed formatting

--- src/microservice/src/main/java/com/coherentsolutions/pot/insurance/controller/NotificationController.java
+++ src/microservice/src/main/java/com/coherentsolutions/pot/insurance/controller/NotificationController.java
@@ -2,7 +2,7 @@ package com.coherentsolutions.pot.insurance.controller;
 
 import com.coherentsolutions.pot.insurance.entity.NotificationEntity;
 import com.coherentsolutions.pot.insurance.service.NotificationService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/notification")
+@RequiredArgsConstructor
 public class NotificationController {
-  @Autowired
-  private NotificationService notificationService;
+
+  private final NotificationService notificationService;
+
   @PostMapping("/send/{mail}")
-  public String sendMail(@PathVariable String mail, @RequestBody NotificationEntity notificationEntity) {
+  public String sendMail(@PathVariable String mail,
+      @RequestBody NotificationEntity notificationEntity) {
     notificationService.sendMail(mail, notificationEntity);
     return "Mail sent successfully";
   }
--- src/microservice/src/main/java/com/coherentsolutions/pot/insurance/service/NotificationService.java
+++ src/microservice/src/main/java/com/coherentsolutions/pot/insurance/service/NotificationService.java
@@ -1,16 +1,17 @@
 package com.coherentsolutions.pot.insurance.service;
 
 import com.coherentsolutions.pot.insurance.entity.NotificationEntity;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class NotificationService {
-  @Autowired
-  private JavaMailSender javaMailSender;
+
+  private final JavaMailSender javaMailSender;
 
   @Value("${spring.mail.username}")
   private String fromMail;
