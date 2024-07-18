e2c2aa161043aade84308e8fa108b6b756a0943e
Lukas Šimonėlis <118674199+Cioperis@users.noreply.github.com>
2024-07-18 18:17:51 +0300

    renamed main application redundant naming

--- microservice/src/main/java/com/coherentsolutions/pot/insurance/NotificationsApplication.java
+++ microservice/src/main/java/com/coherentsolutions/pot/insurance/NotificationsApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-public class NotificationsMicroserviceApplication {
+public class NotificationsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotificationsMicroserviceApplication.class, args);
+		SpringApplication.run(NotificationsApplication.class, args);
 	}
 }
