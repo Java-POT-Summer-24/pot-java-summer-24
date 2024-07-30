4a65a41aea2ea0d28ca58c0495df0fc3db525cfe
dmytromazurev <dmytro.mazurev@icloud.com>
2024-07-30 17:06:09 +0300

    LJI-48. Now total_limit from deactivate plan not added to total_limit sum when we try to create new plan.

--- src/main/java/com/coherentsolutions/pot/insurance/repository/PlanRepository.java
+++ src/main/java/com/coherentsolutions/pot/insurance/repository/PlanRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import com.coherentsolutions.pot.insurance.constants.PlanStatus;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -17,6 +18,6 @@ public interface PlanRepository extends JpaRepository<PlanEntity, UUID>,
 
   Optional<PlanEntity> findById(UUID id);
 
-  @Query("SELECT COALESCE(SUM(p.totalLimit), 0) FROM PlanEntity p WHERE p.packageEntity.id = :packageId")
-  double findSumOfTotalLimitByPackageId(UUID packageId);
+  @Query("SELECT COALESCE(SUM(p.totalLimit), 0) FROM PlanEntity p WHERE p.packageEntity.id = :packageId AND p.status = :status")
+  double findSumOfTotalLimitByPackageId(UUID packageId, PlanStatus status);
 }
